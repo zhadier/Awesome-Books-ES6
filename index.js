cfcc1ba27550c115e5c25d86a11aa9a1fb2b159e
iamgaby <gaby.es.iaa@gmail.com>
2022-01-10 11:56:53 -0500

    Add Date and Time markup in index.js file

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import Collection from "./modules/collection.js";
 import Book from "./modules/book.js";
 import loadNavigation from "./modules/navigation.js";
+import { DateTime } from "./node_modules/luxon/src/luxon.js";
 
 loadNavigation();
 
 const inputTitle = document.getElementById("title");
 const inputAuthor = document.getElementById("author");
 const submitBtn = document.querySelector(".add-btn");
+const dateBox = document.querySelector(".date");
+
+const dt = DateTime.now();
+dateBox.innerHTML = `${dt.monthLong}, ${dt.day} ${dt.weekYear}. ${dt.hour}:${dt.minute}:${dt.second}`;
 
 const coll = new Collection();
 if (localStorage.getItem("bookCollection")) {
@@ -21,3 +26,5 @@ submitBtn.addEventListener("click", () => {
   inputAuthor.value = "";
   inputTitle.value = "";
 });
+
+
